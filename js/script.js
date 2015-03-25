ef1446a0615f478deb265d2b9d8c103de51374a7
Chip Eyler <ceyler@gmail.com>
2015-03-24 21:41:20 -0400

    Added closure

--- js/script.js
+++ js/script.js
@@ -1,25 +1,27 @@
-var app = angular.module('Todo', []);
-app.controller('TodoCtrl', function($scope) {
-  $scope.newTodo = '';
+(function(){
+  var app = angular.module('Todo', []);
+  app.controller('TodoCtrl', function($scope) {
+    $scope.newTodo = '';
 
-  $scope.todos = [
-    /*'Replace faucet',
-    'Pick up steaks',
-    'Call dad',
-    'Get roof quote'*/
-  ];
+    $scope.todos = [
+      /*'Replace faucet',
+      'Pick up steaks',
+      'Call dad',
+      'Get roof quote'*/
+    ];
 
-  $scope.done = function(todo) {
-    var indexOf = $scope.todos.indexOf(todo);
-    if (indexOf !== -1) {
-      $scope.todos.splice(indexOf, 1);
-    }
-  };
+    $scope.done = function(todo) {
+      var indexOf = $scope.todos.indexOf(todo);
+      if (indexOf !== -1) {
+        $scope.todos.splice(indexOf, 1);
+      }
+    };
 
-   $scope.add = function(e) {
-    if (e.which && e.which === 13) {
-      $scope.todos.push($scope.newTodo);
-      $scope.newTodo = '';
-    }
-  };
-});
+     $scope.add = function(e) {
+      if (e.which && e.which === 13) {
+        $scope.todos.push($scope.newTodo);
+        $scope.newTodo = '';
+      }
+    };
+  });
+})();
